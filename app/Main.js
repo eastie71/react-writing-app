ddb73b1702424c279ea04fd184723e3b22091d83
Craig Eastwood <craigdavideastwood@gmail.com>
2020-05-06 23:14:59 +1000

    Initial setup of guest home page

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function ExampleComponent() {
+// Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+
+function Main() {
     return (
-        <div>
-            <h1>This is our app setup!!</h1>
-            <p>The sky is blue and the green is grass!!</p>
-        </div>
+        <>
+            <Header />
+            <HomeGuest />
+            <Footer />
+        </>
     )
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector("#app"))
+ReactDOM.render(<Main />, document.querySelector("#app"))
 
 if (module.hot) {
     module.hot.accept()
