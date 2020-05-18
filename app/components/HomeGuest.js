27c42d1a37032ec0cee07c875aefb67462406147
Craig Eastwood <craigdavideastwood@gmail.com>
2020-05-18 15:32:52 +1000

    Missed Signup button!

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -233,7 +233,7 @@ function HomeGuest() {
                             </CSSTransition>
                         </div>
                         <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
-                            Sign up for ComplexApp
+                            Sign up for WriteApp!
                         </button>
                     </form>
                 </div>
